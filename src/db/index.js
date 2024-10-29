65845974accbcd6cf52f5e7f2371006df25b257a
Vibhanshu Pandey <vibhanshu2207@gmail.com>
2024-10-29 14:46:49 +0530

    corrected typo

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_clNAME}`
+      `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(
       `\n MongoDB Connected! DB HOST: ${connectionInstance.connection.host}` //deployement, production etc all have different hosts, so to know to which host we're connecting to, this connection instance's further methods are useful.
